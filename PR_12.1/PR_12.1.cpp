5ec1bc07ee2f9416f78b35b643221b2e2e7a7044
ievgen275 <ievgen3435@gmail.com>
2024-05-01 19:51:24 +0300

    first commit

---
+++ PR_12.1/PR_12.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Elem {
+	Elem *link1,
+		 *link2;
+	int info;
+};
+
+Elem *p;
+
+int main() {
+	p = new Elem;
+	p->info = 1;
+	p->link1 = new Elem;
+	p->link1->info = 2;
+	p->link1->link1 = new Elem;
+	p->link1->link1->info = 3;
+	p->link2 = new Elem;
+	p->link2->info = 4;
+	p->link1->link2 = p->link2;
+	p->link1->link1->link2 = p->link1->link1;
+	p->link1->link1->link1 = p->link2->link1;
+	p->link2->link2 = p->link1->link1->link2;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_12.1/PR_12.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestNodeCreationAndLinks)
+        {
+			Elem* p;
+			p = new Elem;
+			p->info = 1;
+			p->link1 = new Elem;
+			p->link1->info = 2;
+			p->link1->link1 = new Elem;
+			p->link1->link1->info = 3;
+			p->link2 = new Elem;
+			p->link2->info = 4;
+			p->link1->link2 = p->link2;
+			p->link1->link1->link2 = p->link1->link1;
+			p->link1->link1->link1 = p->link2->link1;
+			p->link2->link2 = p->link1->link1->link2;
+
+			Assert::IsTrue(true, L"All memory deallocated successfully");
+        }
+	};
+}
